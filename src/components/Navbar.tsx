b107471286b6415684a245d82657301f71d49cc3
Javier <javi086mx@gmail.com>
2026-05-06 17:37:58 -0400

    About me section in progress

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,20 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-6 bg-white shadow-sm border-b">
-      <Link href="/" className="text-xl font-bold text-gray-800 tracking-tight">
-        Javi's Portfolio
+    <nav className="flex items-center justify-between p-6 bg-deep-navy shadow-sm  shadow-golden-shadow">
+      <Link href="/" className="text-xl text-primary font-bold  tracking-tight">
+        Javier's Portfolio
       </Link>
       
-      <div className="flex items-center gap-6">
-        <Link href="../app/AboutPage" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">About Me</Link>
-        <Link href="../app/projects" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">Projects</Link>
-        <Link href="/contact" className="text-sm font-medium text-gray-600 hover:text-blue-600 transition-colors">Contact</Link>
-        
+      <div className="flex items-center gap-6 text-secondary">
+        <Link href="/" className="text-base font-medium  hover:text-zinc-300 transition-colors">Home</Link>
         <span className="text-gray-300">|</span>
-        
-        <Link href="/app" className="text-sm font-semibold text-blue-600 hover:text-blue-800 transition-colors">Web App</Link>
-      </div>
+        <Link href="/about" className="text-base font-medium   hover:text-zinc-300 transition-colors">About Me</Link>
+        <span className="text-gray-300">|</span>
+        <Link href="/projects" className="text-base font-medium   hover:text-zinc-300 transition-colors">Projects</Link>
+        <span className="text-gray-300">|</span>
+        <Link href="/contact" className="text-base font-medium   hover:text-zinc-300 transition-colors">Contact</Link>
+        </div>
     </nav>
   );
 }
-

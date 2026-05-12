60ae188c285fdd6e98a7a84757a570c7014a5b7f
Javier <javi086mx@gmail.com>
2026-05-12 11:56:28 -0400

    About me section complited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
     >
-      <body className="min-h-full flex flex-col bg-general-body">
+      <body className="min-h-screen flex flex-col bg-general-body">
         <Navbar />
         {children}
       </body>
---
+++ src/data/projects.ts
@@ -0,0 +1,17 @@
+export const projects =[
+ {
+    id: 1,
+    project_name: "Event Organizer",
+    description: "This project is a reactive Android application built with Kotlin and Jetpack Compose for managing complex schedules. Features include user-specific data isolation, a hierarchical event-task structure, and a smart deadline tracking system. Built using MVVM, Room for persistence, and Flow for real-time UI updates."
+ },   
+ {
+    id:2,
+    project_name: "WBC Store",
+    description: "WBC Store is a front-end e-commerce web application prototype designed to provide a modern, user-friendly shopping experience for curated beverages around the world. Built from scratch using semantic HTML5 and custom CSS3, the project serves as a showcase of modern web design principles, completely independent of external CSS frameworks."
+ },
+ {
+    id:3,
+    project_name: "EasyPass",
+    description: "EasyPass is a web application that simulates a  Password Manager service. It integrates professional grade tools to handle real world scenarios: dynamic product management via Stripe, real-time data fetching through RSS feeds, and a secure administrative backend for transaction tracking."
+ }
+]

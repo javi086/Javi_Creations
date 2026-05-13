e115ecb037ecc8c02b5ed8d679b129447347df13
Javier <javi086mx@gmail.com>
2026-05-13 19:21:10 -0400

    Technologies and initial profile description completed

---
+++ src/data/technologies.ts
@@ -0,0 +1,61 @@
+export const technologies =[
+    {id: 1,
+     name: "React",
+     path: "/icons/reactjs.svg"
+},
+{
+    id: 2,
+    name: "JavaScript",
+    path: "/icons/javascript.svg"
+},
+{
+    id: 3,
+    name: "TypeScript",
+    path: "/icons/typescript.svg"
+},
+{
+    id: 4,
+    name: "Java",
+    path: "/icons/java.svg"
+},
+{
+    id: 5,
+    name: "Kotlin",
+    path: "/icons/kotlin.svg"
+},
+{
+    id: 6,
+    name: "HTML",
+    path: "/icons/html5.svg"
+},
+{
+    id: 7,
+    name: "CSS",
+    path: "/icons/css3.svg"
+},
+{
+    id: 8,
+    name: "Tailwind",
+    path: "/icons/tailwindcss.svg"
+},
+{
+    id: 9,
+    name: "Postman",
+    path: "/icons/postman.svg"
+},
+{
+    id: 10,
+    name: "Postgresql",
+    path: "/icons/postgresql.svg"
+},
+{
+    id: 11,
+    name: "Github",
+    path: "/icons/github.svg"
+},
+{
+    id:12,
+    name:"Jira",
+    path:"/icons/jira.svg"
+}
+]

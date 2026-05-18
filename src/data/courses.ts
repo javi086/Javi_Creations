0e72b0a0b80cd4a46de7eb05724831e8363cf1c9
Javier <javi086mx@gmail.com>
2026-05-18 16:30:58 -0400

    Including diplomas

---
+++ src/data/courses.ts
@@ -0,0 +1,48 @@
+export const courses = [
+    {
+        id:1,
+        name:"Scrum Master",
+        path:"/courses/smc.png"
+    },
+    {
+        id:2,
+        name:"Scrum Fundamentals",
+        path:"/courses/sfc.png"
+    },
+    {   
+        id:3,
+        name:"English",
+        path:"/courses/englishDuolingo.png"
+    },
+    {
+        id:4,
+        name:"English",
+        path:"/courses/englishOne.png"
+    },
+    {
+        id:5,
+        name:"Git & Github",
+        path:"/courses/gitHub.png"
+    },
+    {
+        id:6,
+        name:"Backend Fundamentals",
+        path:"/courses/introBackEnd.png"
+    },
+    {
+        id:7,
+        name:"Object Oriented Programming",
+        path:"/courses/ooo.png"
+    },
+    {
+        id:8,
+        name:"Java Fundamentals",
+        path:"/courses/javaBasics.png"
+    },
+    {
+        id:9,
+        name:"Java OOO",
+        path:"/courses/javaOOO.png"
+    }
+
+]
